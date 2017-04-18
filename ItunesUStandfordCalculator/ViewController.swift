dac8beef9c08d6244db61ef28976d096a39917b4
Matthew Singleton <mattmega4@gmail.com>
2017-04-18 06:02:52 -0700

    Xcode crashed, lost everything, redid half of it

--- ItunesUStandfordCalculator/ViewController.swift
+++ ItunesUStandfordCalculator/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  
+  @IBOutlet weak var display: UILabel!
+  
+  private var brain = CalculatorBrain()
+  
+  var userIsInTheMiddleOfTyping = false
+  
+  var displayValue: Double {
+    get {
+      return Double(display.text!)!
+    }
+    set {
+      display.text = String(newValue)
+    }
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  
+  
+  
+  
+  @IBAction func touchDigit(_ sender: UIButton) {
+    
+    let digit = sender.currentTitle!
+    
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
+      userIsInTheMiddleOfTyping = true
+    }
+    
+    
   }
-
-
+  
+  
+  @IBAction func performOperation(_ sender: UIButton) {
+    
+    if userIsInTheMiddleOfTyping {
+      brain.setOperand(displayValue)
+      userIsInTheMiddleOfTyping = false
+    }
+    
+    if let mathmaticalSymbol = sender.currentTitle {
+      brain.performOperation(mathmaticalSymbol)
+    }
+    
+    if let result = brain.result {
+      displayValue = result
+    }
+    
+  }
+  
+  
 }
 
