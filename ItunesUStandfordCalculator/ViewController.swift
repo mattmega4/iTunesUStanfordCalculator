7a5b870e37eff8231135d1b8512bdf47af98f344
Matthew Singleton <mattmega4@gmail.com>
2017-04-18 03:21:37 -0700

    Initial Commit

---
+++ ItunesUStandfordCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ItunesUStandfordCalculator
+//
+//  Created by Matthew Singleton on 4/18/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
