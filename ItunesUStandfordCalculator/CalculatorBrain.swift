26146daf4532205962882931ca7b83be74fdc463
Matthew Singleton <mattmega4@gmail.com>
2017-04-18 06:01:48 -0700

    first commit

---
+++ ItunesUStandfordCalculator/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  ItunesUStandfordCalculator
+//
+//  Created by Matthew Singleton on 4/18/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
